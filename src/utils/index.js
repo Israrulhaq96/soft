283a23ea51b7def238f17aafea5d4fad4ea10c00
Israrulhaq96 <104883596+Israrulhaq96@users.noreply.github.com>
2023-07-25 18:54:24 +0500

    Delete src directory

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import products from '../jsondata/productList.json'
-
-export function getCategoryProducts(category){
-    const array = products.filter(product => category.cat_id === product.cat_id);
-    // console.log(category, array);
-    return array;
-}
