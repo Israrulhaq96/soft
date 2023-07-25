806f4b9392f717834534c14803500e5125b845cc
Israrulhaq96 <104883596+Israrulhaq96@users.noreply.github.com>
2023-07-25 18:44:27 +0500

    Update MenuPage.js

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import CategoryBar from '../components/CategoryBar';
 import OrderDetails from '../components/OrderDetails';
-import CategoryMenu from '../components/CategoryMenu';
+
 
 export default function MenuPage(props) {
     return (
