abb0c55aa791cda5e979e20cf1c6149e49e14201
Israrulhaq96 <104883596+Israrulhaq96@users.noreply.github.com>
2023-07-25 18:44:06 +0500

    Update MenuPage.js

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -8,7 +8,6 @@ export default function MenuPage(props) {
     return (
         <div className='container'>
             <Header />
-            <CategoryBar />
             <div id="Productmain-height" className="main-height">
                 <div id="Productcontainer-xxl" className="container-xxl category-container-custom mb-md-100">
                     <div className="row" id="Productrow">
